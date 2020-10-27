93d2f18562af11dc550d07eb002abcb318cb0262
Manul <manuljain16@gmail.com>
2020-10-27 20:08:52 +0530

    Added employee attendance

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,7 +3,15 @@ public class EmpWageBuilder {
     public static void main(String[] args) {
         // Welcome message
         System.out.println("welcome to Employee Wage Computation Program.");
-        
+        // Constants
+        int IS_FULL_TIME = 1;
+        // Computation
+        Double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is present.");
+        } else {
+            System.out.println("employee is absent.");
+        }
     }
 
 }
