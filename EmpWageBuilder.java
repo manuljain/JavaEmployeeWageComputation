e80a43b566ea071f085524281a06a9552c6f3720
Manul <manuljain16@gmail.com>
2020-10-27 23:47:33 +0530

    Added employee part time  wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -7,12 +7,18 @@ public class EmpWageBuilder {
         int IS_FULL_TIME = 1;
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int IS_PART_TIME = 2;
+        int partTimeHour = 4;
         // Computation
-        Double empCheck = Math.floor(Math.random() * 10) % 2;
+        Double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME) {
             System.out.println("employee is present.");
             System.out.println("daily wage is : "+wagePerHour*fullDayHour);
-        } else {
+        } else if(empCheck == IS_PART_TIME) {
+            System.out.println("employee is present.");
+            System.out.println("daily part time wage is : "+wagePerHour*partTimeHour);
+        }
+        else {
             System.out.println("employee is absent.");
             System.out.println("daily wage is : 0");
         }
