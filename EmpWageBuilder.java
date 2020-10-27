64addcf33cf81e3780b992eddea48ae727625178
Manul <manuljain16@gmail.com>
2020-10-27 19:44:37 +0530

    Added welcome message

---
+++ EmpWageBuilder.java
@@ -0,0 +1,9 @@
+public class EmpWageBuilder {
+
+    public static void main(String[] args) {
+        // Welcome message
+        System.out.println("welcome to Employee Wage Computation Program.");
+        
+    }
+
+}
