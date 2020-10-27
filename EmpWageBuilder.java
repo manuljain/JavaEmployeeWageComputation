b165078870ca751a625bffb307f1cf2e88e4049e
Manul <manuljain16@gmail.com>
2020-10-27 20:19:12 +0530

    Added employee daily wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,12 +5,16 @@ public class EmpWageBuilder {
         System.out.println("welcome to Employee Wage Computation Program.");
         // Constants
         int IS_FULL_TIME = 1;
+        int wagePerHour = 20;
+        int fullDayHour = 8;
         // Computation
         Double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
             System.out.println("employee is present.");
+            System.out.println("daily wage is : "+wagePerHour*fullDayHour);
         } else {
             System.out.println("employee is absent.");
+            System.out.println("daily wage is : 0");
         }
     }
 
