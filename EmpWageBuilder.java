13a12ee0905fdf836a1b984dd916bce6f1f8e786
Manul <manuljain16@gmail.com>
2020-10-28 10:07:03 +0530

    Added total mothly wage till total working hours or day is reached

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,43 +4,36 @@ public class EmpWageBuilder {
     public static void main(String[] args) {
         // Welcome message
         System.out.println("welcome to Employee Wage Computation Program.");
-        
-        // Creating object of class
-        EmpWageBuilder empw = new EmpWageBuilder();
 
-        // Generating Randoms 0, 1 and 2
-        Random ran= new Random();
-        int empCheck = ran.nextInt(3);
-        
-
-        switch (empCheck){
-        case 0 :
-            System.out.println("employee is present.");
-            empw.partTime();
-            break;
-        case 1 :
-            System.out.println("employee is present.");
-            empw.fullTime();
-            break;
-        case 2 :
-            System.out.println("employee is absent.");
-            System.out.println("daily wage is : 0");
-            break;
-        }
-    }
-    // calculating total month wage
-    public void fullTime(){
+        // defining variables
         int wagePerHour = 20;
         int fullDayHour = 8;
-        int monthlyWorkingDays = 20;
-        int totalMonthWage = wagePerHour*fullDayHour*monthlyWorkingDays;
-        System.out.println("daily full timewage is : "+totalMonthWage);
-    }
-    public void partTime(){
-        int wagePerHour = 20;
         int partTimeHour = 4;
         int monthlyWorkingDays = 20;
-        int totalMonthWage = wagePerHour*partTimeHour*monthlyWorkingDays;
-        System.out.println("daily part time wage is : "+totalMonthWage);
+        int totalWorkingHours = 0;
+
+        for(int i=0;i<monthlyWorkingDays;i++){
+
+            //generating random values 0,1 and 2
+            Random random = new Random();
+            int empCheck = random.nextInt(3);
+
+            if(empCheck == 0){
+               totalWorkingHours += fullDayHour;
+            } else if (empCheck == 1){
+               totalWorkingHours += partTimeHour;
+            } else{
+               totalWorkingHours += 0;   
+            }
+
+            if(totalWorkingHours>=100){
+               totalWorkingHours = 100;
+                break;
+            }
+        }
+        System.out.println("Total Working Hours : " + totalWorkingHours );
+        // calculating total monthly wage
+        int totalMonthlyWage=totalWorkingHours * wagePerHour;
+        System.out.println("Total monthly wage : " + totalMonthlyWage);
     }
 }
