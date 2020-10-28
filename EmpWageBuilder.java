fe08089b75768e139bb94ede548319e1387237e0
Manul <manuljain16@gmail.com>
2020-10-28 09:23:57 +0530

    code using switch case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,27 +1,41 @@
+import java.util.Random;
 public class EmpWageBuilder {
 
     public static void main(String[] args) {
         // Welcome message
         System.out.println("welcome to Employee Wage Computation Program.");
-        // Constants
-        int IS_FULL_TIME = 1;
-        int wagePerHour = 20;
-        int fullDayHour = 8;
-        int IS_PART_TIME = 2;
-        int partTimeHour = 4;
-        // Computation
-        Double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
+        
+        // Creating object of class
+        EmpWageBuilder empw = new EmpWageBuilder();
+
+        // Generating Randoms 0, 1 and 2
+        Random ran= new Random();
+        int empCheck = ran.nextInt(3);
+        
+
+        switch (empCheck){
+        case 0 :
             System.out.println("employee is present.");
-            System.out.println("daily wage is : "+wagePerHour*fullDayHour);
-        } else if(empCheck == IS_PART_TIME) {
+            empw.partTime();
+            break;
+        case 1 :
             System.out.println("employee is present.");
-            System.out.println("daily part time wage is : "+wagePerHour*partTimeHour);
-        }
-        else {
+            empw.fullTime();
+            break;
+        case 2 :
             System.out.println("employee is absent.");
             System.out.println("daily wage is : 0");
+            break;
         }
     }
-
+    public void fullTime(){
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        System.out.println("daily full timewage is : "+wagePerHour*fullDayHour);
+    }
+    public void partTime(){
+        int wagePerHour = 20;
+        int partTimeHour = 4;
+        System.out.println("daily part time wage is : "+wagePerHour*partTimeHour);
+    }
 }
