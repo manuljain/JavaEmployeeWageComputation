54d8499858659cd78742ef9a2e25bfb91b7dbe32
Manul <manuljain16@gmail.com>
2020-11-03 20:30:28 +0530

    Added manage employee wage of multiple companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,26 +1,56 @@
 import java.util.Random;
 
-public class EmpWageBuilder {
+class CompanyEmpWage{
     
     
-    static String company;
-    static int wagePerHour;
-    static int workingDays;
-    static int monthlyHrs;
-    static int totalMonthlyWage;
+    public final String company;
+    public static int wagePerHour;
+    public final int workingDays;
+    public static int monthlyHrs;
+    public int totalMonthlyWage;
     
     //Constructor
-    EmpWageBuilder(String company, int wagePerHour, int workingDays, int monthlyHrs) {
+    public CompanyEmpWage(String company, int wagePerHour, int workingDays, int monthlyHrs) {
         this.company = company;
         this.wagePerHour = wagePerHour;
         this.workingDays = workingDays;
         this.monthlyHrs = monthlyHrs;
     }
+    public void setTotalMonthlyWage(int totalMonthlyWage){
+        this.totalMonthlyWage=totalMonthlyWage;
+    }
+
+    @Override
+    public String toString(){
+        return "Total employee Wage for " + company + " is : "+ totalMonthlyWage;
+    }
+}
+
+public class EmpWageBuilder{
+
     //Computing Wage for Companies
-    static void totalWage() {
-        //Variables
-        int totalWorkingHours=0, fullTimeHrs=8, partTimeHrs=4;
-        for (int i=0; i<workingDays; i++) {
+    private int numOfCompany=0;
+    private CompanyEmpWage[] EmpWageBuilderArray;
+
+    public EmpWageBuilder() {
+        EmpWageBuilderArray = new CompanyEmpWage[5];
+    }
+
+    void addEmpWageBuilder(String company, int wagePerhour, int workingDays, int maxMonthHours){
+        EmpWageBuilderArray[numOfCompany] = new CompanyEmpWage(company, wagePerhour, workingDays,maxMonthHours);
+        numOfCompany++;
+    }
+    void totalWage(){
+        for (int i=0; i<numOfCompany; i++){
+            EmpWageBuilderArray[i].setTotalMonthlyWage(this.totalWage(EmpWageBuilderArray[i]));
+            System.out.println(EmpWageBuilderArray[i]);
+        }
+    }
+    private int totalWage(CompanyEmpWage companyEmpWage) {
+        int totalMonthlyWage=0,totalWorkingHours=0, fullTimeHrs=8, partTimeHrs=4;
+
+        for (int i=0; i<companyEmpWage.workingDays; i++) {
+
             //To Generate Randoms 0, 1 and 2
             Random ran = new Random();
             int empCheck = ran.nextInt(3);
@@ -33,29 +63,28 @@ public class EmpWageBuilder {
             } else {
                 totalWorkingHours += 0;
             }
-            if (totalWorkingHours > monthlyHrs) {
-                totalWorkingHours = monthlyHrs;
+            if (totalWorkingHours > CompanyEmpWage.monthlyHrs) {
+                totalWorkingHours = CompanyEmpWage.monthlyHrs;
                 break;
             }
         }
         //Counting Total Monthly Wage
-        totalMonthlyWage=totalWorkingHours * wagePerHour;
-    }
-    public String toString(){
-        return "Total employee Wage for " + company + " is : "+ totalMonthlyWage;
-    }
+        totalMonthlyWage=totalWorkingHours * CompanyEmpWage.wagePerHour;
     
-
+    
+        return totalMonthlyWage = totalWorkingHours * companyEmpWage.wagePerHour;
+    
+    }
     // Main method
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
         System.out.println("====================================");
-        EmpWageBuilder company1 = new EmpWageBuilder("company1",20,20,100);
-        company1.totalWage();
-        System.out.println(company1);
-        System.out.println("====================================");
-        EmpWageBuilder company2 = new EmpWageBuilder("company2", 50,28, 150);
-        company2.totalWage();
-        System.out.println(company2);
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        empWageBuilder.addEmpWageBuilder("Company1",20,20,100);
+        empWageBuilder.addEmpWageBuilder("Company2", 30,30, 120);
+        empWageBuilder.addEmpWageBuilder("Company3",20,30,100);
+        empWageBuilder.addEmpWageBuilder("Company4", 25,26, 120);
+        empWageBuilder.totalWage();
+        
     }
 }
